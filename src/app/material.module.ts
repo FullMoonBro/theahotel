54f9d2ea3d53ede83eb275043347606422611e6e
fullmoonbro <realbadur@live.com>
2018-12-22 16:18:36 +0400

    Initial Commit, created routing module, added hotel pages, added paths

---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import {
+    MatBadgeModule,
+    MatButtonModule
+ } from '@angular/material';
+
+@NgModule ({
+    imports: [MatBadgeModule,
+        MatButtonModule
+    ],
+    exports: [MatBadgeModule,
+        MatButtonModule
+    ]
+})
+
+export class MaterialModule {
+
+}
